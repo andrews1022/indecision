195c2845d859b10af7e23aae8b16f6908794ed6b
andrews1022 <andrewshearerdev@gmail.com>
2022-02-13 15:17:14 -0800

    setup styled components

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,20 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+// styled components
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './styles/GlobalStyle';
+import theme from './styles/theme';
+
+// components
+import App from './components/App';
 
 ReactDOM.render(
 	<StrictMode>
-		<App />
+		<ThemeProvider theme={theme}>
+			<GlobalStyle />
+			<App />
+		</ThemeProvider>
 	</StrictMode>,
 	document.getElementById('root')
 );
